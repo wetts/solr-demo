d846a4b6185a31ba904c758c04bf3d48293085c8
wetts <zhang.wetts@163.com>
2017-03-10 17:37:46 +0800

    init project

---
+++ solr6/src/main/java/com/wetts/Main.java
@@ -0,0 +1,13 @@
+package com.wetts;
+
+/**
+ * Created by wetts on 2017/3/10.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SolrService solrService = new SolrService();
+
+        solrService.query("");
+    }
+}
---
+++ solr6/src/main/java/com/wetts/TestBean.java
@@ -0,0 +1,26 @@
+package com.wetts;
+
+/**
+ * Created by wetts on 2017/3/10.
+ */
+public class TestBean {
+
+    private String name;
+    private String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
